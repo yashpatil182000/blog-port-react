734207a8318d1dfc0ed6aaa7c27500188d5f2d14
Yash Patil <yashpatil182000@gmail.com>
2025-01-31 14:22:46 +0530

    initial commit | project setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-3xl underline bg-primaryBG">
+        Blog Port | ReactJS | Firebase
+      </p>
+    </>
+  );
+}
+
+export default App;
